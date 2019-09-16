4fe8f8f1529aa0577eba5d22f22010266549d957
Abed Zantout <abed@techhive.io>
2019-09-16 17:12:28 +0300

    feat(app): added navigation back to home

--- shared/components/header/index.tsx
+++ shared/components/header/index.tsx
@@ -1,13 +1,17 @@
 import React, { FunctionComponent } from 'react';
+import Link from 'next/link';
 
 type Props = {}
 const Header: FunctionComponent<Props> = ({}) => {
     return (
         <div className="nav">
-            <img className="logo" src="/static/logo.png" alt="logo"/>
+            <Link href="/">
+                <img className="logo" src="/static/logo.png" alt="logo"/>
+            </Link>
             <style jsx>{`
                 .nav { background-color: #ffffff; border-bottom: 1px solid #ececec; box-shadow: 0 2px 50px 0 rgba(0,0,0,0.16);}
-                .logo { border-radius: 50%; width: 10rem; }
+                .logo { border-radius: 50%; width: 10rem; };
+                .logo:hover { cursor: pointer; };
             `}</style>
         </div>
     );
