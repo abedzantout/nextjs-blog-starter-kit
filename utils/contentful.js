940d0a2424332c53bbb746266c0ca2f7a9b509d8
Abed Zantout <abed@techhive.io>
2019-09-19 11:20:07 +0300

    chore(app): added contentful tag creation

--- utils/contentful.js
+++ utils/contentful.js
@@ -1,122 +1,154 @@
 const authorFields = [
-  {
-    id: 'name',
-    name: 'Name',
-    type: 'Symbol',
-    required: true
-  },
-  {
-    id: 'title',
-    name: 'Title',
-    type: 'Symbol',
-    required: true
-  },
-  {
-    id: 'company',
-    name: 'Company',
-    type: 'Symbol',
-    required: true
-  },
-  {
-    id: 'shortBio',
-    name: 'Short Bio',
-    type: 'Text',
-    required: true
-  },
-  {
-    id: 'email',
-    name: 'Email',
-    type: 'Symbol',
-    required: true
-  },
-  {
-    id: 'twitter',
-    name: 'Twitter',
-    type: 'Symbol',
-    required: true
-  }
+    {
+        id: 'name',
+        name: 'Name',
+        type: 'Symbol',
+        required: true
+    },
+    {
+        id: 'title',
+        name: 'Title',
+        type: 'Symbol',
+        required: true
+    },
+    {
+        id: 'company',
+        name: 'Company',
+        type: 'Symbol',
+        required: true
+    },
+    {
+        id: 'shortBio',
+        name: 'Short Bio',
+        type: 'Text',
+        required: true
+    },
+    {
+        id: 'email',
+        name: 'Email',
+        type: 'Symbol',
+        required: true
+    },
+    {
+        id: 'twitter',
+        name: 'Twitter',
+        type: 'Symbol',
+        required: true
+    }
 ];
 
 const blogFields = [
-  {
-    id: 'title',
-    name: 'Title',
-    type: 'Symbol',
-    required: true
-  },
-  {
-    id: 'slug',
-    name: 'Slug',
-    type: 'Symbol',
-    required: true
-  },
-  {
-    id: 'heroImage',
-    name: 'Hero Image',
-    type: 'Object',
-    required: true
-  },
-  {
-    id: 'description',
-    name: 'Description',
-    type: 'Text',
-    required: true
-  },
-  {
-    id: 'body',
-    name: 'Body',
-    type: 'Text',
-    required: true
-  },
-  {
-    id: 'author',
-    name: 'Author',
-    type: 'Link',
-    required: true,
-    linkType: 'Entry',
-    validations: [{ linkContentType: ['Author'] }]
-  },
-  {
-    id: 'publishedDate',
-    name: 'Published Date',
-    type: 'Date',
-    required: true
-  }
+    {
+        id: 'title',
+        name: 'Title',
+        type: 'Symbol',
+        required: true
+    },
+    {
+        id: 'slug',
+        name: 'Slug',
+        type: 'Symbol',
+        required: true
+    },
+    {
+        id: 'heroImage',
+        name: 'Hero Image',
+        type: 'Object',
+        required: true
+    },
+    {
+        id: 'description',
+        name: 'Description',
+        type: 'Text',
+        required: true
+    },
+    {
+        id: 'body',
+        name: 'Body',
+        type: 'Text',
+        required: true
+    },
+    {
+        id: 'author',
+        name: 'Author',
+        type: 'Link',
+        required: true,
+        linkType: 'Entry',
+        validations: [{linkContentType: ['author']}]
+    },
+    {
+        id: 'publishedDate',
+        name: 'Published Date',
+        type: 'Date',
+        required: true
+    },
+    {
+        id: 'tags',
+        name: 'Tags',
+        type: 'Link',
+        required: true,
+        linkType: 'Array',
+        validations: [{linkContentType: ['tag']}]
+    },
 ];
 
-module.exports = function(migration, context) {
-  const author = migration.createContentType('Author', {
-    name: 'Author',
-    description: 'Author of Blogpost'
-  });
+const tagFields = [
+    {
+        id: 'name',
+        name: 'Name',
+        type: 'Symbol',
+        required: true
+    },
+];
 
-  authorFields.forEach(field => {
-    author.createField(field.id, {
-      name: field.name,
-      type: field.type,
-      required: field.required
+module.exports = function (migration, context) {
+    const tag = migration.createContentType('tag', {
+        name: 'Tag',
+        description: 'tag'
     });
-  });
-  const blog = migration.createContentType('Blog', {
-    name: 'Blog',
-    description: 'blogpost'
-  });
 
-  blogFields.forEach(field => {
-    if (!field.linkType) {
-      blog.createField(field.id, {
-        name: field.name,
-        type: field.type,
-        required: field.required
-      });
-    } else {
-      blog.createField(field.id, {
-        name: field.name,
-        type: field.type,
-        linkType: field.linkType,
-        required: field.required,
-        validations: field.validations
-      });
-    }
-  });
+    tagFields.forEach(field => {
+        tag.createField(field.id, {
+            name: field.name,
+            type: field.type,
+            required: field.required
+        });
+    });
+
+    const author = migration.createContentType('author', {
+        name: 'Author',
+        description: 'Author of Blogpost'
+    });
+
+    authorFields.forEach(field => {
+        author.createField(field.id, {
+            name: field.name,
+            type: field.type,
+            required: field.required
+        });
+    });
+    const blog = migration.createContentType('blogPost', {
+        name: 'BlogPost',
+        description: 'Blog Post'
+    });
+
+    blogFields.forEach(field => {
+        if (!field.linkType) {
+            blog.createField(field.id, {
+                name: field.name,
+                type: field.type,
+                required: field.required
+            });
+        } else {
+            blog.createField(field.id, {
+                name: field.name,
+                type: field.type,
+                linkType: field.linkType,
+                required: field.required,
+                validations: field.validations
+            });
+        }
+    });
+
+
 };
