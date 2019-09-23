91177638a7c3b47b9ef4b32f4b58aa0375f1a672
Abed Zantout <abed@techhive.io>
2019-09-23 10:23:13 +0300

    refactor(card): move helper functions to own file

--- shared/components/card/card.component.tsx
+++ shared/components/card/card.component.tsx
@@ -1,15 +1,12 @@
-import React, {FunctionComponent, Fragment} from 'react';
+import React, { FunctionComponent } from 'react';
 import './card.component.css';
 import Link from 'next/link';
+import { getHref, getNavigationLink } from '../../helpers/helper';
 
 type Props = {
     info: { id: string, title: string; description: string; heroImage: string; publishedAt: Date; slug: string }
 }
-const getNavigationLink = (slug) => `/post/${slug}`;
-const getHref = (slug) => ({
-    pathname: '/post',
-    query: {post: slug},
-});
+
 const Card: FunctionComponent<Props> = ({info}) => {
 
     const cardBGStyles = {
@@ -18,19 +15,19 @@ const Card: FunctionComponent<Props> = ({info}) => {
     };
 
     return (
-            <div className="card">
-                <div className="card__header" style={cardBGStyles}/>
-                <div className="card__body">
-                    <h3 className="card__title">{info.title}</h3>
-                    <p className="card__text">{info.description}</p>
-                </div>
-
-                <div className="card__footer">
-                    <Link href={getHref(info.slug)} as={getNavigationLink(info.slug)}>
-                        <a className="card__action">Explore</a>
-                    </Link>
-                </div>
+        <div className="card">
+            <div className="card__header" style={cardBGStyles}/>
+            <div className="card__body">
+                <h3 className="card__title">{info.title}</h3>
+                <p className="card__text">{info.description}</p>
             </div>
+
+            <div className="card__footer">
+                <Link href={getHref(info.slug)} as={getNavigationLink(info.slug)}>
+                    <a className="card__action">Explore</a>
+                </Link>
+            </div>
+        </div>
     );
 };
 
