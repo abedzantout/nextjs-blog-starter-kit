3d33ba6e25743c5667ae002a7d8d02efd1f97f86
Abed Zantout <abed@techhive.io>
2020-07-10 14:30:46 +0300

    fix(header): added new TH logo

--- shared/components/header/header.component.tsx
+++ shared/components/header/header.component.tsx
@@ -9,7 +9,7 @@ const Header: FunctionComponent<Props> = ({}) => {
     <div className={styles.nav}>
       <div className={styles.brand}>
         <Link href="/">
-          <img className={styles.brand__logo} src="/static/logo.png" alt="logo" />
+          <img className={styles.brand__logo} src="https://www.techhive.io/static/brand/logo-masterclass.svg" alt="logo" />
         </Link>
 
         <h3 className={styles.brand__name}>TechHive.IO: NextJs Static Starter Kit.</h3>
