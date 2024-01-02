ae8b83faae7f5cbf001a375c71c377941ff22759
Abdul Rahman Zantout <abdulrahman@farmlend.co.uk>
2024-01-02 19:56:19 +0000

    chore(app): remove deprecated export

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
 const next_config = {
+  output: 'export',
   swcMinify: true,
   webpack: (config) => {
     config.plugins = config.plugins || [];
